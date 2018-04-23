4f8decdfa522ad165ccf5bc5c8f93352e8b3654f
UnknownFrost <38655543+UnknownFrost@users.noreply.github.com>
2018-04-23 16:21:32 +0200

    Ajout projet

---
+++ Dogfight/src/jpu2016/dogfight/view/DogfightView.java
@@ -0,0 +1,20 @@
+package jpu2016.dogfight.view;
+
+public class DogfightView {
+
+	public DogfightView(IOrderPerformer orderPerformer, IDogfightModel dogfightModel, Observable observable) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private void run() {
+		
+	}
+	public String displayMessage(String message) {
+		return message;
+	}
+	
+	public void closeAll() {
+		
+	}
+
+}
---
+++ Dogfight/src/jpu2016/dogfight/view/GraphicsBuilder.java
@@ -0,0 +1,32 @@
+package jpu2016.dogfight.view;
+
+import java.awt.image.*;
+import jpu2016.dogfight.model;
+
+
+
+public class GraphicsBuilder {
+		
+	public GraphicsBuilder(final IDogfightModel dogfightmodel) {
+		// TODO Auto-generated constructor stub
+			
+	}
+	public void applyModelToGraphic(final Graphics graphics, final ImageObserver observer) {
+		
+		
+	}
+	private void buildEmptySky() {
+		
+	}
+	
+	private void drawMobile(final IMoblie mobile, final Graphics graphics, final ImageObserver observer) {
+		
+	}
+	 public int getGlobalWidth() {
+		 return 5;
+	 }
+	 
+	 public int getGlobalHeight() {
+		 return 5;
+	 }
+}
