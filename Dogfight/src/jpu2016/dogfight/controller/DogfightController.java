e2f43e3ef46f378d73fc9d021462602f134149ac
Nathan <Nathan@LAPTOP-VVN0DVM6>
2018-04-24 15:00:40 +0200

    Phase 2/3 Controller

---
+++ Dogfight/src/jpu2016/dogfight/controller/DogfightController.java
@@ -0,0 +1,36 @@
+package jpu2016.dogfight.controller;
+import jpu2016.dogfight.view.*;
+
+public class DogfightController implements IOrderPerformer {
+
+	private static int TIME_SLEEP = 30;
+	
+	public DogfightController(IDogfightModel dofighModel) {
+		
+	}
+	
+	public void orderPerformer(UserOrder userOrder) {
+		
+	}
+	
+	public void play()
+	{
+		
+	}
+	
+	public void setViewSystem(IViewSystem viewSystem)
+	{
+		
+	}
+	
+	private void lauchMissile(int player)
+	{
+		
+	}
+	
+	private void gameLoop()
+	{
+		
+	}
+
+}
